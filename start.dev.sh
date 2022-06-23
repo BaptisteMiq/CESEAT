25b8e5394aa2e1a18a947f606eb749dc4783f935
BaptisteMiq <baptistemi2000@gmail.com>
2022-06-23 10:06:48 +0200

    App start scripts

---
+++ start.dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CURRENT_DIR="$(echo "$(pwd)")"
+APPS=()
+
+APPS+=("Application"    "cd Application && npm run dev")
+APPS+=("Database"       "cd Database && docker compose build && docker compose down && docker compose up")
+APPS+=("MSC Accounts"   "cd Microservices/Accounts && npm run dev")
+APPS+=("MSC CDN"        "cd Microservices/CDN && npm run dev")
+APPS+=("Middleware"     "cd Middleware && npm run watch")
+
+for ((i = 0; i < ${#APPS[@]}; i++)); do
+    cd $CURRENT_DIR
+    gnome-terminal --title="${APPS[$i]}" --working-directory=$CURRENT_DIR -x bash -c "${APPS[$i+1]}; bash"
+    i=$((i+1))
+done
