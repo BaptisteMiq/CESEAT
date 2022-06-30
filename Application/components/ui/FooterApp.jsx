5726f860227ab1dc2b9c83c384854eb42319196d
florian <flo47rossi@gmail.com>
2022-06-30 20:37:34 +0200

    Fix footer / delivery

--- Application/components/ui/FooterApp.jsx
+++ Application/components/ui/FooterApp.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     var history = useHistory();
  return (
     <div className='h-20 border bg-black absolute w-full flex bottom-0 shadow-xl align-center text-center flex-col justify-center'>
-      <div style={{color: "white"}}>CES'EAT - Tout droit réservé</div>
+      <div style={{color: "white"}}>CES'EAT - Tous droits réservés</div>
       <StyledLink style={{color: "white"}} href="" onClick={(e) => {
                 history.push('/users/legal-mentions');
               }}>
